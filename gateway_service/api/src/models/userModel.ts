c5c9fab9866bc0c7c8c02d20f99a52e43f1bf1e3
issam <issame.hassane@hotmail.com>
2024-11-03 14:59:04 +0100

    fit : ajout du dossier pour la gateway

---
+++ gateway_service/api/src/models/userModel.ts
@@ -0,0 +1,28 @@
+import {RowDataPacket} from "mysql2/promise";
+
+/**
+ * Interface compte utilisateur
+ */
+interface UserInterface {
+    id_user: number;
+    email: string;
+    password: string;
+    firstname: string;
+    lastname: string;
+    // TODO reste de l'interface
+}
+
+/**
+ * Mapper compte utilisateur
+ */
+function rowToUserInterface (row: RowDataPacket): UserInterface {
+    return {
+        id_user: row['id_user'],
+        email: row['email'],
+        password: row['password'],
+        firstname: row['firstname'],
+        lastname: row['lastname']
+    };
+}
+
+export {UserInterface, rowToUserInterface};
